d2f6300d72c984ae3d609ee4248cc86ffecab492
bbmcca26@g.holycross.edu <bbmcca26@g.holycross.edu>
2024-08-22 18:08:40 -0700

    refactor: make endpoint detection implementation object-oriented

---
+++ pyfishsensedev/fish/fish_geometry.py
@@ -0,0 +1,215 @@
+import numpy as np
+import cv2
+import shapely
+from shapely import ops
+
+class FishGeometry:
+    def __init__(self, mask: np.ndarray):
+        self.mask = mask
+        self.perimeter = None
+        self.pca_endpoints = None # the estimated endpoints from PCA (client sets this) TODO: rename
+        self.ab = None # the line connecting the endpoints 
+        self.ab_perp = None # the line perpendicular to ab 
+        self.polygon = None # a polygonal representation of the fish mask 
+        self.halves = None # two polygon halves sliced by ab_perp 
+        self.halves_convex = None # the respective convex hulls of the halves
+        self.tail_coord = None # the estimated tail coord from PCA after binary classification (client sets this)
+        self.head_coord = None # the estimated head coord from PCA after binary classification (clients sets this)
+        self.tail_poly = None # the polygon half containing tail_coord (client sets this)
+        self.head_poly = None # the polygon half containing head_coord (client sets this)
+        # ^^ The client only needs to classify either the coords or the polygons. They don't have to do both.
+        # The respective counterparts are calculated automatically.
+        self.headpoint_line = None # a line parallel to ab_perp with its centroid being head_coord
+        self.tailpoint_line = None # a line parallel to ab_perp with its centroid being tail_coord
+        self.nose_point = None # a point further out from the head_coord
+        self.tail_point = None # a point further out form the tail_coord
+        self.head_corrected = None # the corrected head point (client sets this)
+        self.tail_corrected = None # the corrected tail point (client sets this)
+
+    def set_endpoints(self, endpoints):
+        self.pca_endpoints = endpoints
+    
+    def set_tail_poly(self, tail_poly):
+        self.tail_poly = tail_poly
+
+    def set_head_poly(self, head_poly):
+        self.head_poly = head_poly
+
+    def set_tail_coord(self, tail_coord):
+        self.tail_coord = tail_coord
+
+    def set_head_coord(self, head_coord):
+        self.head_coord = head_coord
+
+    def set_head_corrected(self, head_corrected):
+        self.head_corrected = head_corrected
+
+    def set_tail_corrected(self, tail_corrected):
+        self.tail_corrected = tail_corrected
+
+    def get_endpoints(self):
+        assert self.pca_endpoints is not None, "You need to set the endpoints first! Use set_endpoints()."
+        return self.pca_endpoints
+    
+    def get_perimeter(self):
+        if self.perimeter is not None:
+            return self.perimeter
+        contours, _ = cv2.findContours(self.mask.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        self.perimeter = contours[0].reshape(-1, 2)
+        return self.perimeter
+    
+    def get_ab(self):
+        if self.ab is not None:
+            return self.ab
+        self.ab = shapely.geometry.LineString(self.get_endpoints())
+        return self.ab
+    
+    def get_ab_perp(self):
+        if self.ab_perp is not None:
+            return self.ab_perp
+        ab = self.get_ab()
+        perimeter = self.get_perimeter()
+        vert_min = abs(perimeter[:,1].min() - ab.centroid.y)
+        vert_max = abs(perimeter[:,1].max() - ab.centroid.y)
+        vert_len = vert_min if vert_min > vert_max else vert_max
+        ab_left = ab.parallel_offset(vert_len*1.5, 'left')
+        ab_right = ab.parallel_offset(vert_len*1.5, 'right')
+        self.ab_perp = shapely.geometry.LineString([ab_left.centroid, ab_right.centroid])
+        return self.ab_perp
+    
+    def get_polygon(self):
+        if self.polygon is not None:
+            return self.polygon
+        self.polygon = shapely.geometry.Polygon(self.get_perimeter())
+        return self.polygon
+    
+    def get_halves(self):
+        if self.halves is not None:
+            return self.halves
+        self.halves = ops.split(self.get_polygon(), self.get_ab_perp()).geoms
+        if len(self.halves) > 2:
+            self.halves = sorted(self.halves, key=lambda p: p.area, reverse=True)[:2]
+        return self.halves
+    
+    def get_halves_convex(self):
+        if self.halves_convex is not None:
+            return self.halves_convex
+        halves = self.get_halves()
+        self.halves_convex = [halves[0].convex_hull, halves[1].convex_hull]
+        return self.halves_convex
+    
+    def get_head_poly(self):
+        if self.head_poly is not None:
+            return self.head_poly
+        self.head_poly, _ = self.__find_polys_from_matching_endpoints()
+        return self.head_poly
+    
+    def get_tail_poly(self):
+        if self.tail_poly is not None:
+            return self.tail_poly
+        _, self.tail_poly = self.__find_polys_from_matching_endpoints()
+        return self.tail_poly
+    
+    def get_head_coord(self, endpoints=None):
+        if self.head_coord is not None:
+            return self.head_coord
+        self.head_coord, _ = self.__find_endpoints_from_matching_polys(endpoints)
+        return self.head_coord
+    
+    def get_tail_coord(self, endpoints=None):
+        if self.tail_coord is not None:
+            return self.tail_coord
+        _, self.tail_coord = self.__find_endpoints_from_matching_polys(endpoints)
+        return self.tail_coord
+    
+    def get_nose_point(self):
+        if self.nose_point is not None:
+            return self.nose_point
+        self.__find_outer_points()
+        return self.nose_point
+    
+    def get_tail_point(self):
+        if self.tail_point is not None:
+            return self.tail_point
+        self.__find_outer_points()
+        return self.tail_point
+    
+    def get_headpoint_line(self):
+        if self.headpoint_line is not None:
+            return self.headpoint_line
+        self.__find_endpoint_lines()
+        return self.headpoint_line
+
+    def get_tailpoint_line(self):
+        if self.tailpoint_line is not None:
+            return self.tailpoint_line
+        self.__find_endpoint_lines()
+        return self.tailpoint_line
+    
+    def get_head_corrected(self):
+        assert self.head_corrected is not None, "You need to set head_corrected. Try set_head_corrected()."
+        return self.head_corrected
+    
+    def get_tail_corrected(self):
+        assert self.tail_corrected is not None, "You need to set tail_corrected. Try set_tail_corrected()."
+        return self.tail_corrected
+
+    def __find_endpoint_lines(self):
+        head_coord = self.get_head_coord()
+        tail_coord = self.get_tail_coord()
+        ab = self.get_ab()
+        ab_perp = self.get_ab_perp()
+        # get the perpendicular line of ab from the headpoint
+        half_len = shapely.distance(ab, shapely.Point(head_coord).centroid)
+        endpoint_line1 = ab_perp.parallel_offset(half_len, 'right')
+        endpoint_line2 = ab_perp.parallel_offset(half_len, 'left')
+        # again choose the right line
+        if (shapely.distance(endpoint_line1.centroid, shapely.Point(head_coord)) < 
+            shapely.distance(endpoint_line1.centroid, shapely.Point(tail_coord))):
+            self.headpoint_line = endpoint_line1
+            self.tailpoint_line = endpoint_line2
+        else:
+            self.headpoint_line = endpoint_line2
+            self.tailpoint_line = endpoint_line1
+        
+    def __find_outer_points(self):
+        ab_perp = self.get_ab_perp()
+        perimeter = self.get_perimeter()
+        # draw two lines parallel to ab_perp a little bit aways from the original endpoints
+        hor_min = abs(perimeter[:,0].min() - ab_perp.centroid.x)
+        hor_max = abs(perimeter[:,0].max() - ab_perp.centroid.x)
+        hor_len = hor_min if hor_min > hor_max else hor_max
+        nose_line1 = ab_perp.parallel_offset(hor_len*2.0, 'right') # hor_len*2.0
+        nose_line2 = ab_perp.parallel_offset(hor_len*2.0, 'left') # hor_len*2.0
+        # choose the line near the head and get the centroid 
+        if (shapely.distance(nose_line1.centroid, shapely.Point(self.get_head_coord())) < 
+            shapely.distance(nose_line1.centroid, shapely.Point(self.get_tail_coord()))):
+            self.nose_point = nose_line1.centroid
+            self.tail_point = nose_line2.centroid
+        else:
+            self.nose_point = nose_line2.centroid
+            self.tail_point = nose_line1.centroid
+    
+    def __find_endpoints_from_matching_polys(self, endpoints=None):
+        assert self.head_poly is not None and self.tail_poly is not None, "You need to classify the polygons. Try set_head_poly() or set_tail_poly()."
+        left_coord, right_coord = self.pca_endpoints if endpoints == None else endpoints
+        if (abs(shapely.distance(shapely.Point(left_coord), self.get_head_poly())) <
+            abs(shapely.distance(shapely.Point(left_coord), self.get_tail_poly()))):
+            head_coord = left_coord
+            tail_coord = right_coord
+        else:
+            head_coord = right_coord
+            tail_coord = left_coord
+        return head_coord, tail_coord
+    
+    def __find_polys_from_matching_endpoints(self):
+        assert self.head_coord is not None and self.tail_coord is not None, "You need to classify the endpoints. Try set_head_coord() or set_tail_coord()."
+        halves = set.get_halves()
+        if (abs(shapely.distance(shapely.Point(self.head_coord), halves[0])) <
+            abs(shapely.distance(shapely.Point(self.head_coord), halves[1]))):
+            head_poly = halves[0]
+            tail_poly = halves[1]
+        else:
+            head_poly = halves[1]
+            tail_poly = halves[0]
+        return head_poly, tail_poly
--- pyfishsensedev/fish/fish_head_tail_detector.py
+++ pyfishsensedev/fish/fish_head_tail_detector.py
@@ -8,145 +8,79 @@ import cv2
 import shapely
 from shapely import ops
 
-class FishGeometry:
-    def __init__(self, mask: np.ndarray):
-        self.mask = mask
-        self.perimeter = self.get_perimeter()
-        self.pca_endpoints = None # the estimated endpoints from PCA 
-        self.ab = None # the line connecting the endpoints 
-        self.ab_perp = None # the line perpendicular to ab 
-        self.polygon = None # a polygonal representation of the fish mask 
-        self.halves = None # two polygon halves sliced by ab_perp 
-        self.halves_convex = None # the respective convex hulls of the halves
-        self.halves_difference = None # the polygons contained in halves_convex but not halves 
-        self.tail_coord = None # the estimated tail coord from PCA after binary classification
-        self.head_coord = None # the estimated head coord from PCA after binary classification
-        self.tail_poly = None # the polygon half containing tail_coord
-        self.head_poly = None # the polygon half containing head_coord
-        self.headpoint_line = None # a line parallel to ab_perp with its centroid being head_coord
-        self.tailpoint_line = None # a line parallel to ab_perp with its centroid being tail_coord
-        self.nose_point = None # a point further out from the head_coord
-        self.tail_point = None # a point further out form the tail_coord
-        self.head_corrected = None
-        self.tail_corrected = None
-    
-    def get_perimeter(self):
-        # find the perimeter of the mask
-        contours, _ = cv2.findContours(self.mask.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        self.perimeter = contours[0].reshape(-1, 2)
+from pyfishsensedev.fish.fish_geometry import FishGeometry
 
 class FishHeadTailDetector:
     def __init__(self, mask: np.ndarray):
         self.geo = FishGeometry(mask)
 
+    def correct_tail_coord(self): # TODO
+        self.geo.set_tail_corrected(self.geo.get_tail_coord())
+
+        return {
+            'tail_corrected': np.asarray(self.geo.get_tail_corrected())
+            }
+
     def correct_head_coord(self):
-        # draw two lines parallel to ab_perp a little bit aways from the original endpoints
-        hor_min = abs(self.geo.perimeter[:,0].min() - self.geo.ab_perp.centroid.x)
-        hor_max = abs(self.geo.perimeter[:,0].max() - self.geo.ab_perp.centroid.x)
-        hor_len = hor_min if hor_min > hor_max else hor_max
-        nose_line1 = self.geo.ab_perp.parallel_offset(hor_len*2.0, 'right') # hor_len*2.0
-        nose_line2 = self.geo.ab_perp.parallel_offset(hor_len*2.0, 'left') # hor_len*2.0
-
-        # choose the line near the head and get the centroid 
-        if (shapely.distance(nose_line1.centroid, shapely.Point(self.geo.head_coord)) < 
-            shapely.distance(nose_line1.centroid, shapely.Point(self.geo.tail_coord))):
-            self.geo.nose_point = nose_line1.centroid
-            self.geo.tail_point = nose_line2.centroid
-        else:
-            self.geo.nose_point = nose_line2.centroid
-            self.geo.tail_point = nose_line1.centroid
-
-        # get the perpendicular line of ab from the headpoint
-        half_len = shapely.distance(self.geo.ab.centroid, shapely.Point(self.geo.head_coord).centroid)
-        endpoint_line1 = self.geo.ab_perp.parallel_offset(half_len, 'right')
-        endpoint_line2 = self.geo.ab_perp.parallel_offset(half_len, 'left')
-
-        # again choose the right line
-        if (shapely.distance(endpoint_line1.centroid, shapely.Point(self.geo.head_coord)) < 
-            shapely.distance(endpoint_line1.centroid, shapely.Point(self.geo.tail_coord))):
-            self.geo.headpoint_line = endpoint_line1
-            self.geo.tailpoint_line = endpoint_line2
-        else:
-            self.geo.headpoint_line = endpoint_line2
-            self.geo.tailpoint_line = endpoint_line1
-
-        # slice the headpoly by that line
         # any point on head_poly_sliced is better or just as good as the initial estimated point
-        head_poly_sliced = ops.split(self.geo.head_poly, self.geo.headpoint_line).geoms
+        head_poly_sliced = ops.split(self.geo.get_head_poly(), self.geo.get_headpoint_line()).geoms
+
         # polygon closest to nosepoint is the one we want
-        distances = [shapely.distance(p, self.geo.nose_point) for p in head_poly_sliced]
+        distances = [shapely.distance(p, self.geo.get_nose_point()) for p in head_poly_sliced]
         head_poly_sliced = head_poly_sliced[distances.index(min(distances))]
 
         # we guess the tip of head_poly_sliced by finding its nearest point to another point far ahead
         try:
-            _, head_corrected = ops.nearest_points(self.geo.nose_point, head_poly_sliced.convex_hull.boundary)
+            _, head_corrected = ops.nearest_points(self.geo.get_nose_point(), head_poly_sliced.convex_hull.boundary)
         except:
             # if a point can't be extracted, we default to the original estimation
-            head_corrected = shapely.geometry.Point(self.geo.head_coord)
-        self.geo.head_corrected = [head_corrected.x, head_corrected.y]
+            head_corrected = shapely.geometry.Point(self.geo.get_head_coord())
+        self.geo.set_head_corrected([head_corrected.x, head_corrected.y])
 
         return {
-            'head_corrected': np.asarray(self.geo.head_corrected)
+            'head_corrected': np.asarray(self.geo.get_head_corrected())
             }
     
     def correct_endpoints(self):
         self.correct_head_coord()
+
+        self.correct_tail_coord()
+
         return {
-            'head_corrected': np.asarray(self.geo.head_corrected)
+            'head_corrected': np.asarray(self.geo.get_head_corrected()),
+            'tail_corrected': np.asarray(self.geo.get_tail_corrected())
             }
 
-    def classify_endpoints(self) -> Dict[str, np.ndarray]:
-        left_coord, right_coord = self.geo.pca_endpoints
+    def classify_endpoints(self, endpoints=None) -> Dict[str, np.ndarray]:
+        endpoints = self.geo.get_endpoints() if endpoints == None else endpoints
+        self.geo.set_endpoints(endpoints)
 
-        # find the perimeter of the mask
-        contours, _ = cv2.findContours(self.geo.mask.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        self.geo.perimeter = contours[0].reshape(-1, 2)
-
-        # find the line perpendicular to the line connecting the points
-        self.geo.ab = shapely.geometry.LineString([left_coord, right_coord])
-        vert_min = abs(self.geo.perimeter[:,1].min() - self.geo.ab.centroid.y)
-        vert_max = abs(self.geo.perimeter[:,1].max() - self.geo.ab.centroid.y)
-        vert_len = vert_min if vert_min > vert_max else vert_max
-        ab_left = self.geo.ab.parallel_offset(vert_len*1.5, 'left')
-        ab_right = self.geo.ab.parallel_offset(vert_len*1.5, 'right')
-        self.geo.ab_perp = shapely.geometry.LineString([ab_left.centroid, ab_right.centroid])
-
-        # create a polygon
-        self.geo.polygon = shapely.geometry.Polygon(self.geo.perimeter)
-
-        # split the polygon by the perpendicular line
-        self.geo.halves = ops.split(self.geo.polygon, self.geo.ab_perp).geoms
-        if len(self.geo.halves) > 2:
-            self.geo.halves = sorted(self.geo.halves, key=lambda p: p.area, reverse=True)[:2]
+        # get halves
+        halves = self.geo.get_halves()
 
         # get the convex versions
-        self.geo.halves_convex = [self.geo.halves[0].convex_hull, self.geo.halves[1].convex_hull]
+        halves_convex = self.geo.get_halves_convex()
 
         # get the differences
-        self.geo.halves_difference = [shapely.difference(self.geo.halves_convex[0], self.geo.halves[0]),
-                             shapely.difference(self.geo.halves_convex[1], self.geo.halves[1])]
+        halves_difference = [shapely.difference(halves_convex[0], halves[0]),
+                             shapely.difference(halves_convex[1], halves[1])]
 
-        # compare the areas
-        half_areas = [self.geo.halves_difference[0].area], [self.geo.halves_difference[1].area]
-        self.geo.tail_poly = self.geo.halves[half_areas.index(max(half_areas))]
-        self.geo.head_poly = self.geo.halves[half_areas.index(min(half_areas))]
+        # compare the areas and set head/tail polys
+        half_areas = [halves_difference[0].area], [halves_difference[1].area]
+        self.geo.set_tail_poly(halves[half_areas.index(max(half_areas))])
+        self.geo.set_head_poly(halves[half_areas.index(min(half_areas))])
 
-        # determine which coord is head/tail
-        if (abs(shapely.distance(shapely.Point(left_coord), self.geo.head_poly)) <
-            abs(shapely.distance(shapely.Point(left_coord), self.geo.tail_poly))):
-            self.geo.head_coord = left_coord
-            self.geo.tail_coord = right_coord
-        else:
-            self.geo.head_coord = right_coord
-            self.geo.tail_coord = left_coord
+        # assign the classification to the endpoints
+        head_coord = self.geo.get_head_coord(endpoints)
+        tail_coord = self.geo.get_tail_coord(endpoints)
 
         # compute the confidence score
         confidence = float((max(half_areas)[0] - min(half_areas)[0]) / max(half_areas)[0] / 2.0 + 0.5)
         confidence = int(confidence * 100) / 100
 
         return {
-                'head': np.asarray(self.geo.head_coord),
-                'tail': np.asarray(self.geo.tail_coord),
+                'head': np.asarray(head_coord),
+                'tail': np.asarray(tail_coord),
                 'confidence': confidence
                 }
 
@@ -214,7 +148,7 @@ class FishHeadTailDetector:
         left_coord[1] += y_min
         right_coord[1] += y_min
 
-        self.geo.pca_endpoints = (left_coord, right_coord)
+        self.geo.set_endpoints([left_coord, right_coord])
         return self.geo.pca_endpoints
 
 if __name__ == "__main__":
